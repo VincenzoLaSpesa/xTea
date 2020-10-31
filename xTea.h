273e41d826047ed3dc54faf5e1426370e619a009
Vincenzo La Spesa <vincenzolaspesa@gmail.com>
2020-11-01 00:10:47 +0100

    Create c-cpp.yml (#2)
    * Create c-cpp.yml
    
    Testing the building action
    
    * syntax check with clang-format and building pipeline
    
    * Trying to build for windows too
    
    * Create build-release.yml (#3)
    
    * Actions work, almost
    
    * cleaning action

--- xTea.h
+++ xTea.h
@@ -7,43 +7,44 @@
 
 #pragma once
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 /**
 * PCKS5 padding (https://www.ietf.org/rfc/rfc1423.txt https://www.di-mgt.com.au/cryptopad.html )
 */
 class StreamPadder {
-  public:
-    StreamPadder(std::ifstream *stream);
-    ~StreamPadder();
-    int ReadBlockPad(uint64_t *blocco);
-    int ReadBlock(uint64_t *blocco);
-    void Close();
-    short UnPad(const char *data);
-  private:
-    std::ifstream *reader_;
-    short pad_ = 0;
+ public:
+  StreamPadder(std::ifstream *stream);
+  ~StreamPadder();
+  int ReadBlockPad(uint64_t *blocco);
+  int ReadBlock(uint64_t *blocco);
+  void Close();
+  short UnPad(const char *data);
+
+ private:
+  std::ifstream *reader_;
+  short pad_ = 0;
 };
 
 class xTea {
-  public:
-    static const uint64_t iv=0x1234567890abcdef;
-    static const int round=64;
+ public:
+  static const uint64_t iv = 0x1234567890abcdef;
+  static const int round = 64;
 
-    xTea();
-    ~xTea();
+  xTea();
+  ~xTea();
 
-    bool Setup(const char *input, const char *output,const uint32_t *chiave);
-    bool Encode(bool cbc=false);
-    bool Decode(bool cbc=false);
-    bool Dup(bool pad);
-  private:
-    void encipher(uint64_t *blocco, bool cbc);
-    void decipher(uint64_t *blocco, bool cbc);
-    uint32_t chiave_[4];
-    uint64_t oldblock_;
-    std::ifstream reader_;
-    std::ofstream writer_;
+  int Setup(const char *input, const char *output, const uint32_t *chiave);
+  bool Encode(bool cbc = false);
+  bool Decode(bool cbc = false);
+  bool Dup(bool pad);
+
+ private:
+  void encipher(uint64_t *blocco, bool cbc);
+  void decipher(uint64_t *blocco, bool cbc);
+  uint32_t chiave_[4];
+  uint64_t oldblock_;
+  std::ifstream reader_;
+  std::ofstream writer_;
 };
-
